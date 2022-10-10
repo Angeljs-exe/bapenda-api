d1164617ad69877e4250f598c244bdcef655797b
tendeanclaudior <tendeanclaudio1234@gmail.com>
2022-10-10 19:02:36 +0800

    revisi google login

--- src/utils/PushNotif/index.js
+++ src/utils/PushNotif/index.js
@@ -1,14 +1,15 @@
-import React, { useEffect } from 'react'
-import firebase from '@react-native-firebase/app'
-import messaging from '@react-native-firebase/messaging'
+import React, {useEffect} from 'react';
+import firebase from '@react-native-firebase/app';
+import messaging from '@react-native-firebase/messaging';
 
 const PushNotif = () => {
-    useEffect(() => {
-        messaging().getToken(firebase.app().options.messagingSenderId)
-        .then((token) => {
-            console.log('token', token)
-        })
-    }, [])
-}
+  useEffect(() => {
+    messaging()
+      .getToken(firebase.app().options.messagingSenderId)
+      .then(token => {
+        console.log('token', token);
+      });
+  }, []);
+};
 
-export default PushNotif
+export default PushNotif;
